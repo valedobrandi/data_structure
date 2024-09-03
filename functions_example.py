4bdae114d30a3287e76c548acd324fe3bd5fa937
valedobrandi <valedobrandi@gmail.com>
2024-09-03 08:07:43 -0300

    functions

---
+++ functions_example.py
@@ -0,0 +1,26 @@
+array = [1, 2, 3, 4, 5]
+
+other_array = [7, 8, 9]
+
+new_array = array + other_array
+
+print(new_array)
+print('---------')
+
+print(5 in new_array)
+print(10 in new_array)
+
+print(new_array.count(1))
+
+matrix = [[1, 2, 3],
+
+          [4, 5, 6],
+
+          [7, 8, 9]]
+print(matrix)
+
+print(matrix[1][1])
+
+del matrix[1][1]
+
+print(matrix)
